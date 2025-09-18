f3bee6952fb77a3572690c4c9a23b41e144466a7
lnz <192272978+lorenzus-jpeg@users.noreply.github.com>
2025-09-19 01:30:55 +0200

    Initial Commit

---
+++ Radio Teate On Air/Radio_Teate_On_AirApp.swift
@@ -0,0 +1,17 @@
+//
+//  Radio_Teate_On_AirApp.swift
+//  Radio Teate On Air
+//
+//  Created by Lorenzo Cugini on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Radio_Teate_On_AirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
